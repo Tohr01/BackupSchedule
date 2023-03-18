c703f5d3b9cd5a77c104f2c3ab893110372428a9
Tohr01 <carl@kratzundraabe.de>
2023-03-18 17:25:57 +0100

    Initial Commit

---
+++ BackupSchedule/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+// AppDelegate.swift
+// BackupSchedule
+//
+// Created by Tohr01 on 18.03.23
+// Copyright © 2023 Tohr01. All rights reserved.
+//
+        
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
